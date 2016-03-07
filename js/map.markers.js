5e009e47b822ecd07932b17f1e8d88fc372966c2
Fadi Eliya <fadi.eliya@digitalscientists.com>
2016-03-07 08:26:05 -0500

    Update map markers

--- js/map.markers.js
+++ js/map.markers.js
@@ -1,52 +1,52 @@
 //List of map markers
 var map_markers = [
 	{
-		title: "Venice",
-		latitude: 45.436356,
-		longitude: 12.3280503,
+		title: "Ceremony",
+		latitude: 45.4354405,
+		longitude: 12.3426153,
+		icon: "fa-bell-o",
+		infoWindow: "Chiesa di San Giorgio dei Greci <br> Campo dei Greci, 3422, 30100 Castello, Venezia VE"
+	},
+	{
+		title: "Reception",
+		latitude: 45.4427588,
+		longitude: 12.3347175,
 		icon: "fa-glass",
-		infoWindow: "Venice"
+		infoWindow: "Residenza Ca Zanardi <br> Calle Zanardi Cannaregio, Venezia"
 	},
-	{
-		"title": "Reception",
-		"latitude":33.775664,
-		"longitude":-118.065090,
-		"icon":"fa-glass", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
-		"infoWindow":"Old Ranch Country Club <br> 29033 West Lake Drive, Agoura Hills, CA"
-	},
-	{
-		"title": "Accommodation 1",
-		"latitude":33.777929,
-		"longitude":-118.076891,
-		"icon":"fa-bed", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
-		"infoWindow":"Accommodation 1 <br> Rossmoor, CA 90720"
-	},
-	{
-		"title": "Accommodation 2",
-		"latitude":33.780428,
-		"longitude":-118.084075,
-		"icon":"fa-bed", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
-		"infoWindow":"Accommodation 2 <br> Los Alamitos, CA 90720"
-	},
-	{
-		"title": "Accommodation 3",
-		"latitude":33.777551,
-		"longitude":-118.050468,
-		"icon":"fa-bed", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
-		"infoWindow":"Accommodation 3 <br> Seal Beach, CA 90740"
-	},
-	{
-		"title": "Transportation",
-		"latitude":33.782930,
-		"longitude":-118.060552,
-		"icon":"fa-plane", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
-		"infoWindow":"Seal Beach VORTAC SLI <br> Los Alamitos, CA 90720"
-	},
-	{
-		"title": "Ceremony",
-		"latitude":33.776384,
-		"longitude":-118.058149,
-		"icon":"fa-bell-o", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
-		"infoWindow":"Birchwood Church <br> Seal Beach, CA 90740"
-	}
+	// {
+	// 	"title": "Accommodation 1",
+	// 	"latitude":33.777929,
+	// 	"longitude":-118.076891,
+	// 	"icon":"fa-bed", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+	// 	"infoWindow":"Accommodation 1 <br> Rossmoor, CA 90720"
+	// },
+	// {
+	// 	"title": "Accommodation 2",
+	// 	"latitude":33.780428,
+	// 	"longitude":-118.084075,
+	// 	"icon":"fa-bed", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+	// 	"infoWindow":"Accommodation 2 <br> Los Alamitos, CA 90720"
+	// },
+	// {
+	// 	"title": "Accommodation 3",
+	// 	"latitude":33.777551,
+	// 	"longitude":-118.050468,
+	// 	"icon":"fa-bed", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+	// 	"infoWindow":"Accommodation 3 <br> Seal Beach, CA 90740"
+	// },
+	// {
+	// 	"title": "Transportation",
+	// 	"latitude":33.782930,
+	// 	"longitude":-118.060552,
+	// 	"icon":"fa-plane", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+	// 	"infoWindow":"Seal Beach VORTAC SLI <br> Los Alamitos, CA 90720"
+	// },
+	// {
+	// 	"title": "Ceremony",
+	// 	"latitude":33.776384,
+	// 	"longitude":-118.058149,
+	// 	"icon":"fa-bell-o", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+	// 	"infoWindow":"Birchwood Church <br> Seal Beach, CA 90740"
+	// }
 ];
